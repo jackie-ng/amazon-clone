7339431b10af3223618b0ad7dd64a20b5614642c
Trang Nguyen <ntttrang2130@gmail.com>
2022-04-26 23:19:04 -0400

    add react-router-dom & firebase config

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Amazon Clone</h1>
-    </div>
+    <Router>
+      <div className="app">
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/checkout">Check Out</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> ---> <Routes> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Routes>
+          <Route path="/checkout">
+          </Route>
+          <Route path="/login">
+          </Route>
+          <Route path="/">
+          </Route>
+        </Routes>
+      </div>
+    </Router>
   );
 }
-
 export default App;
